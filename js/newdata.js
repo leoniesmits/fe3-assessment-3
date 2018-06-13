dc681199230ae58ca054aa251db64d7acc0d7154
leoniesmits <leonie_smits@live.nl>
2018-06-13 13:04:43 +0200

    updated styling for responsive

---
+++ js/newdata.js
@@ -0,0 +1,16 @@
+var sentimentData=[
+{year:'2005',sentiment:{negative:2, neutral:2, positive:4}}
+,{year:'2006',sentiment:{negative:0, neutral:3, positive:6}}
+,{year:'2007',sentiment:{negative:0, neutral:4, positive:3}}
+,{year:'2008',sentiment:{negative:0, neutral:3, positive:6}} 
+,{year:'2009',sentiment:{negative:1, neutral:2, positive:4}}
+,{year:'2010',sentiment:{negative:0, neutral:5, positive:7}}
+,{year:'2011',sentiment:{negative:1, neutral:7, positive:8}}
+,{year:'2012',sentiment:{negative:0, neutral:3, positive:14}}	
+,{year:'2013',sentiment:{negative:2, neutral:11, positive:19}} 
+,{year:'2014',sentiment:{negative:1, neutral:5, positive:16}} 
+,{year:'2015',sentiment:{negative:11, neutral:8, positive:12}} 
+,{year:'2016',sentiment:{negative:14, neutral:8, positive:4}}
+];
+
+makeCharts('#field',sentimentData);
